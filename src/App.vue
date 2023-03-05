5e8b077eecac7da6eccc3593e2f7210996166d79
Luke <ljhorner2@gmail.com>
2023-03-05 15:04:59 +0000

    Installing bulma

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <header>
+    <div>insert logo</div>
     <div>Welcome to the Username Generator!</div>
   </header>
 
   <main>
-    <div>Please input your username and nationality</div>
+    <div>Please input your username</div>
   </main>
 </template>
 
