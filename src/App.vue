73ee0a5e8035ff985d256fe10e3951567354c7bd
Luke <ljhorner2@gmail.com>
2023-03-05 15:37:47 +0000

    bulma style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <header>
-    <div>insert logo</div>
-    <div>Welcome to the Username Generator!</div>
+    <section class="section">
+      <div class="title">Welcome to the Username Generator!</div>
+    </section>
   </header>
 
   <main>
-    <div>Please input your username</div>
+    <section class="section is-level">
+
+      <div class="field">
+        <div class="subtitle">Please input your username</div>
+        <div class="control">
+          <input class="input" type="text">
+        </div>
+      </div>
+      <button class="level-right button is-warning">Generate Alternatives</button>
+    </section>
   </main>
 </template>
 
