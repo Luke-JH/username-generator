59569b3cf24a9d21629a9a3c5e9a34cccae87fdc
Luke <ljhorner2@gmail.com>
2023-03-14 14:55:10 +0000

    footer

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,13 @@
       <button class="level-right button is-rounded is-primary">Generate Alternatives</button>
     </section>
   </main>
+<div class="section is-medium"></div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p><strong>copyright © 2023</strong></p>
+      <a href="https://github.com/Luke-JH/username-generator">Check out the Github repo here</a>
+    </div>
+  </footer>
 </template>
 
 
