16d855ce2ba583fda567ce5447c507d45735415f
Luke <ljhorner2@gmail.com>
2023-04-14 17:44:21 +0100

    Copy button and indentation

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,66 @@
 <template>
 
-  <header>
-    <nav class="hero is-light">
-        <img class="container image is-128x128" src="src\assets\logo.png">
-    </nav>
+	<header>
+		<nav class="hero is-light">
+			<img class="container image is-128x128" src="src\assets\logo.png">
+		</nav>
 
-  </header>
+	</header>
 
-  <main>
-    <section class="section">
-      <div class="title">Welcome to the Username Generator!</div>
-      <div class="field">
-        <div class="subtitle">Please input your username</div>
-        <div class="control">
-          <input class="input  is-rounded column is-one-third" type="text" v-model="this.name">
-        </div>
-      </div>
-      <button class="level-right button is-rounded is-primary" @click="this.nameGen(this.name, this.additions)">
-        Generate Alternatives
-      </button>
-      <div class="tile is-vertical is-light">
-        <li v-for="item in this.newNames">{{ item }}</li>
-      </div>
-    </section>
-  </main>
+	<main>
+		<section class="section">
+			<div class="title">Welcome to the Username Generator!</div>
+			<div class="field">
+				<div class="subtitle">Please input your username</div>
+				<div class="control">
+				<input class="input  is-rounded column is-one-third" type="text" v-model="this.name">
+				</div>
+			</div>
+			<button class="level-right button is-rounded is-primary" @click="this.nameGen(this.name, this.additions)">
+				Generate Alternatives
+			</button>
+			<div class="tile is-vertical is-light">
+				<CopyButton v-for="item in this.newNames" :text=item />
+			</div>
+		</section>
+	</main>
 
-  <copybutton/>
 
-  <section class="section is-large"></section>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p><strong>copyright © 2023</strong></p>
-      <p>Thank you for using the username generator :)</p>
-    </div>
-  </footer>
-  
-</template>
+	<section class="section is-large"></section>
+	<footer class="footer">
+		<div class="content has-text-centered">
+		<p><strong>copyright © 2023</strong></p>
+		<p>Thank you for using the username generator :)</p>
+		</div>
+	</footer>
+	
+	</template>
 
-<script>
+	<script>
 
-  import copybutton from './components/copy-button.vue'
+	import CopyButton from './components/CopyButton.vue'
 
-  export default {
-    components: {
-      copybutton
-    },
-    data(){
-      return {
-        name: '',
-        additions: ['.','-','_'],
-        newNames: []
-      }
-    },
-    methods: {
-      nameGen(name, additions){
-        this.newNames = []
-        additions.forEach((item) => {
-          let newName = name.concat(item)
-          this.newNames = [...this.newNames, newName]
-        })
-        console.log(this.newNames)
-      }
-    }
-}
-</script>
+	export default {
+		name: 'App',
+		components: {
+		CopyButton
+		},
+		data(){
+		return {
+			name: '',
+			additions: ['.','-','_'],
+			newNames: [],
+		}
+		},
+		methods: {
+		nameGen(name, additions){
+			this.newNames = []
+			additions.forEach((item) => {
+			let newName = name.concat(item)
+			this.newNames = [...this.newNames, newName]
+			})
+			console.log(this.newNames)
+		}
+		}
+	}
+	</script>
---
+++ src/components/CopyButton.vue
@@ -0,0 +1,35 @@
+	<template>
+		<section class="section">
+			<div class="columns">
+			<div class="column is-one-third">
+				<input class="input is-rounded"
+					v-on:focus="$event.target.select()" 
+					ref="clone" 
+					readonly 
+					:value="this.text"/>
+			</div>
+			<div class="column">
+				<button class="button is-rounded is-primary" @click="copy">Copy</button>
+			</div>
+			</div>
+		</section>
+
+	</template>
+	
+	<script>
+
+	export default {
+		name: 'CopyButton',
+		props: {
+			text: {
+				type: String,
+			}
+		},
+		methods: {
+		copy() {
+			this.$refs.clone.focus();
+			document.execCommand('copy');
+		}
+		}
+	}
+	</script>
