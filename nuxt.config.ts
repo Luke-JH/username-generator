ca7c2d13715133a7534954541704ca9556e6cf51
Luke <ljhorner2@gmail.com>
2025-02-16 15:38:15 +0000

    moving tailwind css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     ssr: false,
-    css: ["assets/css/tailwind.css"],
+    css: ["assets/tailwind.css"],
     modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
 });
