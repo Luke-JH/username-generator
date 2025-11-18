dd4be2901e32bc185a9adfa53b99e32b24359546
Luke Horner <ljhorner2@gmail.com>
2025-11-18 16:10:37 +0000

    create BaseButton component

---
+++ components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button
+        :class="`flex ${stackContent && 'flex-col gap-1'} items-center button rounded-lg bg-primary hover:bg-primary-hover text-white p-3`"
+        @click="$emit('clicked')"
+    >
+        <p v-if="text">{{ text }}</p>
+        <font-awesome v-if="icon" :icon="icon" size="lg" />
+    </button>
+</template>
+<script setup lang="ts">
+interface Props {
+    text?: string;
+    icon?: [string, string];
+    stackContent?: boolean;
+}
+defineProps<Props>();
+</script>
