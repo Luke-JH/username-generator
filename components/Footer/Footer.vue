3ef0c61cc5dff234d043583bddbecdba79f8d838
Luke Horner <ljhorner2@gmail.com>
2025-11-20 16:15:38 +0000

    footer rework

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,6 +1,8 @@
 <template>
     <footer class="fixed bottom-0 py-6 bg-gray-100 w-full flex flex-col items-center text-center">
-        <p class="font-bold">{{ `copyright © ${new Date().getFullYear()}` }}</p>
-        <p>Thank you for using the username generator, have a nice day.</p>
+        <p class="font-bold">{{ `MIT Licensed ${new Date().getFullYear()}` }}</p>
+        <a href="https://github.com/Luke-JH/username-generator" class="underline text-gray-500">
+            See the GitHub Repo here
+        </a>
     </footer>
 </template>
